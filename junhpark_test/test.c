8bfa7d36bc37cdeb95430648daa5502f45cb7dfb
feldblume5263 <feldblume5263@gmail.com>
2020-12-26 19:32:41 +0900

    fork test update

---
+++ junhpark_test/test.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+typedef struct	s_env
+{
+	char		pwd[100];
+}				t_env;
+
+
+int			main(void)
+{
+	t_env	env;
+	char	input[100];
+	pid_t	child_pid;
+	pid_t	wait_pid;
+	int		status;
+
+	strlcpy(env.pwd, "~/mac/os/dir/file/", 100);
+	while(1)
+	{
+		printf("bash-3.2$ ");
+		scanf("%s", input);
+		if (strcmp(input, "pwd") == 0)
+		{
+			child_pid = fork();
+			wait_pid = wait(&status);
+			if (child_pid == 0)
+			{
+				printf("%s\n", env.pwd);
+				exit(0);
+			}
+		}
+	}
+}
