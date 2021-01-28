513409da4301e4767a60110a59c9bf9b3198184a
feldblume5263 <feldblume5263@gmail.com>
2021-01-28 17:35:36 +0900

    update whitespace distributor

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 17:43:48 by junhpark          #+#    #+#             */
-/*   Updated: 2021/01/28 15:10:13 by junhpark         ###   ########.fr       */
+/*   Updated: 2021/01/28 15:47:34 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,9 +48,42 @@ int			find_system_error(char **cmds) // *수정
 	return (1);
 }
 
+int			count_add_space(char *cmd)
+{
+	int		idx;
+	int		count;
+
+	idx = -1;
+	count = 0;
+	while (cmd[++idx])
+	{
+		if (cmd[idx] == '>')
+		{
+			if (idx > 0 && cmd[idx - 1] != (char)SPACE\
+				&& cmd[idx - 1] != '<' && cmd[idx - 1] != '>')
+				count++;
+			if (cmd[idx + 1] && cmd[idx - 1] != (char)SPACE\
+				&& cmd[idx + 1] != '<' && cmd[idx + 1] != '>')
+				count++;)
+		}
+	}
+}
+
+void		insert_appro_space(char **cmd)
+{
+	char	*res;
+
+	if (*cmd == NULL || ft_strlen(*cmd) == 0)
+		return ;
+	if (!(res = (char *)malloc(sizeof(char) *\
+		(ft_strlen(*cmd) + count_add_space(*cmd) + 1))))
+		return ;
+}
+
 int			divide_cmds(char ***cmds, char **cmd)
 {
 	refine_cmd(cmd);
+	insert_appro_space(cmd);
 	(*cmds) = ft_split(*cmd, (char)DIV);
 	if (find_system_error(*cmds) < 0) // *수정
 	{
