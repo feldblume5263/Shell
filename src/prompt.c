c36a2f8ca77f366d8bad8c24078d0b5cd2588f57
feldblume5263 <feldblume5263@gmail.com>
2021-01-29 22:58:58 +0900

    update input prehandle code but it occurs memory error

--- src/prompt.c
+++ src/prompt.c
@@ -6,7 +6,7 @@
 /*   By: junhpark <junhpark@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/18 17:43:48 by junhpark          #+#    #+#             */
-/*   Updated: 2021/01/28 15:47:34 by junhpark         ###   ########.fr       */
+/*   Updated: 2021/01/29 22:57:30 by junhpark         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,34 +50,81 @@ int			find_system_error(char **cmds) // *수정
 
 int			count_add_space(char *cmd)
 {
-	int		idx;
-	int		count;
+	int		idx[2];
 
-	idx = -1;
-	count = 0;
-	while (cmd[++idx])
+	idx[0] = -1;
+	idx[1] = 0;
+	while (cmd[++idx[0]])
 	{
-		if (cmd[idx] == '>')
+		if (!is_closed(cmd, idx[0]) && cmd[idx[0]] == '>'\
+			&& cmd[idx[0] + 1] && cmd[idx[0] + 1] == '>')
 		{
-			if (idx > 0 && cmd[idx - 1] != (char)SPACE\
-				&& cmd[idx - 1] != '<' && cmd[idx - 1] != '>')
-				count++;
-			if (cmd[idx + 1] && cmd[idx - 1] != (char)SPACE\
-				&& cmd[idx + 1] != '<' && cmd[idx + 1] != '>')
-				count++;)
+			if (idx[0] > 0 && cmd[idx[0] - 1] != (char)SPACE)
+				idx[1]++;
+			if (cmd[idx[0] + 2] && cmd[idx[0] + 2] != (char)SPACE)
+				idx[1]++;
+			idx[0]++;
+		}
+		else if (!is_closed(cmd, idx[0]) && (cmd[idx[0]] == '>' ||\
+			cmd[idx[0]] == '<' || cmd[idx[0]] == '|'))
+		{
+			if (idx[0] > 0 && cmd[idx[0] - 1] != (char)SPACE)
+				idx[1]++;
+			if (cmd[idx[0] + 1] && cmd[idx[0] + 1] != (char)SPACE)
+				idx[1]++;
 		}
 	}
+	return (idx[1]);
+}
+
+void		reload_line(char *cmd, char **res)
+{
+	int		idx;
+	int		res_idx;
+
+	idx = -1;
+	res_idx = -1;
+	while (cmd[++idx])
+	{
+		if (is_closed(cmd, idx) && cmd[idx] == '>'\
+			&& cmd[idx + 1] && cmd[idx + 1] == '>')
+		{
+			if (idx > 0 && cmd[idx - 1] != (char)SPACE)
+				(*res)[++res_idx] = (char)SPACE;
+			(*res)[++res_idx] = cmd[idx];
+			(*res)[++res_idx] = cmd[idx + 1];
+			if (cmd[idx + 2] && cmd[idx + 2] != (char)SPACE)
+				(*res)[++res_idx] = (char)SPACE;
+			idx++;
+		}
+		else if (is_closed(cmd, idx) && (cmd[idx] == '>' ||\
+			cmd[idx] == '<' || cmd[idx] == '|'))
+		{
+			if (idx > 0 && cmd[idx - 1] != (char)SPACE)
+				(*res)[++res_idx] = (char)SPACE;
+			(*res)[++res_idx] = cmd[idx];
+			if (cmd[idx + 1] && cmd[idx + 1] != (char)SPACE)
+				(*res)[++res_idx] = (char)SPACE;
+		}
+		else
+			(*res)[++res_idx] = cmd[idx];
+	}
+	(*res)[++res_idx] = '\0';
 }
 
 void		insert_appro_space(char **cmd)
 {
 	char	*res;
+	int		size;
 
 	if (*cmd == NULL || ft_strlen(*cmd) == 0)
 		return ;
-	if (!(res = (char *)malloc(sizeof(char) *\
-		(ft_strlen(*cmd) + count_add_space(*cmd) + 1))))
+	size = ft_strlen(*cmd) + count_add_space(*cmd);
+	if (!(res = (char *)malloc(sizeof(char) * (size + 1))))
 		return ;
+	reload_line(*cmd, &res);
+	free(*cmd);
+	*cmd = res;
 }
 
 int			divide_cmds(char ***cmds, char **cmd)
