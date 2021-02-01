dcc0a21b4424149be63343936c2bc3eb23b891e7
yeoyeoyoe <yeoyeoyoe@gmail.com>
2021-02-02 05:17:44 +0900

    cd maybe...?

--- src/cd.c
+++ src/cd.c
@@ -6,11 +6,52 @@
 /*   By: kyeo <kyeo@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/10 20:38:37 by kyeo              #+#    #+#             */
-/*   Updated: 2021/01/31 15:12:38 by kyeo             ###   ########.fr       */
+/*   Updated: 2021/02/02 05:15:25 by kyeo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void
+	replace_pwd_data(t_shell *sptr, t_env *temp,\
+						const char *new_dir, const char *pwd_type)
+{
+	temp = find_env_by_name(sptr->env, pwd_type);
+	free_ptr(&(temp->data));
+	temp->data = ft_strdup(new_dir);
+}
+
+void
+	set_dir(char *dir)
+{
+	ft_memset(dir, 0, PATH_MAX);
+	getcwd(dir, PATH_MAX);
+}
+
+int
+	change_old_and_cur(t_shell *sptr, const char *new_dirname)
+{
+	t_env			*new;
+	t_env			*old;
+
+	if (*new_dirname == '-' && ft_strlen(new_dirname) == 1)
+	{
+		new = find_env_by_name(sptr->env, "PWD");
+		old = find_env_by_name(sptr->env, "OLDPWD");
+		new_dirname = ft_strdup(old->data);
+		free_ptr(&(old->data));
+		old->data = ft_strdup(new->data);
+		free_ptr(&(new->data));
+		new->data = ft_strdup(new_dirname);
+		write(1, new_dirname, ft_strlen(new_dirname));
+		write(1, "\n", 1);
+		if (chdir(new_dirname) == -1)
+			perror("mini: cd");
+		free_ptr((char **)&new_dirname);
+		return (1);
+	}
+	return (0);
+}
 
 void
 	builtins_cd(t_shell *sptr, char *new_dirname)
@@ -18,21 +59,20 @@ void
 	char			cur_dirname[PATH_MAX];
 	t_env			*temp;
 
-	ft_memset(cur_dirname, 0, PATH_MAX);
-	getcwd(cur_dirname, PATH_MAX);
-	temp = find_env_by_name(sptr->env, "OLDPWD");
-	free(temp->data);
-	temp->data = ft_strdup(cur_dirname);
-	if (new_dirname == (char *)0)
+	if (change_old_and_cur(sptr, new_dirname) == 1)
+		return ;
+	temp = 0;
+	set_dir(cur_dirname);
+	replace_pwd_data(sptr, temp, cur_dirname, "OLDPWD");
+	if (new_dirname == (char *)0 ||\
+		(*new_dirname == '~' && ft_strlen(new_dirname) == 1))
 	{
 		temp = find_env_by_name(sptr->env, "HOME");
-		chdir(temp->data);
+		if (chdir(temp->data) == -1)
+			perror("mini: cd");
 	}
-	else
-		chdir(new_dirname);
-	temp = find_env_by_name(sptr->env, "PWD");
-	free(temp->data);
-	ft_memset(cur_dirname, 0, PATH_MAX);
-	getcwd(cur_dirname, PATH_MAX);
-	temp->data = ft_strdup(cur_dirname);
+	else if (chdir(new_dirname) == -1)
+			perror("mini: cd");
+	set_dir(cur_dirname);
+	replace_pwd_data(sptr, temp, cur_dirname, "PWD");
 }
